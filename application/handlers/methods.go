8c71aa92d8c5b6d29064c020a72de8816c312db4
Rony <sirony514@gmail.com>
2026-01-05 00:13:09 +0600

    feat: add write data struct for response in write methods

--- application/handlers/methods.go
+++ application/handlers/methods.go
@@ -1,18 +1,59 @@
 package handlers
 
 import (
-	"encoding/base64"
-	"fmt"
+	"encoding/json"
+	"errors"
 
+	"github.com/rnium/rhttp/internal/http/form"
+	"github.com/rnium/rhttp/internal/http/headers"
 	"github.com/rnium/rhttp/internal/http/request"
 	"github.com/rnium/rhttp/internal/http/response"
 )
 
-func HandlePost(r *request.Request) *response.Response {
-	ctype, _ := r.Headers.Get("content-type")
-	res_str := ""
-	if ctype == "image/png" {
-		res_str = fmt.Sprintf("data: %s", base64.StdEncoding.EncodeToString(r.Body))
+type stringMap map[string]string
+
+type WriteData struct {
+	Args    stringMap `json:"args"`
+	Data    string    `json:"data"`
+	Files   stringMap `json:"files"`
+	Form    stringMap `json:"form"`
+	Headers stringMap `json:"headers"`
+	Json    stringMap `json:"json"`
+	Origin  string    `json:"origin"`
+	Url     string    `json:"url"`
+}
+
+func newWriteData() *WriteData {
+	return &WriteData{
+		Args:    make(stringMap),
+		Files:   make(stringMap),
+		Form:    make(stringMap),
+		Headers: make(stringMap),
 	}
-	return response.NewResponse(200, []byte(res_str), nil)
+}
+
+func getWriteData(req *request.Request) *WriteData {
+	wd := newWriteData()
+	req.Headers.ForEach(func(name, value string) {
+		wd.Headers[name] = value
+	})
+	formdata, _ := form.GetFormData(req)
+	if formdata != nil {
+		wd.Form = formdata.Fields
+		for field, file := range formdata.Files {
+			wd.Files[field] = file.ToBase64Data()
+		}
+	}
+	return wd
+}
+
+func HandlePost(r *request.Request) *response.Response {
+	wd := getWriteData(r)
+	data, err := json.Marshal(wd)
+	if err != nil {
+		return response.Response500(errors.New("Cannot convert data to json"))
+	}
+	headers := headers.NewHeaders()
+	_ = headers.Set("Content-type", "application/json")
+	return response.NewResponse(200, data, headers)
 }
